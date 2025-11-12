fb6f9b22d3a24e155134ae71e55907048647d1ec
Elizabeth Omigie <omigieelizabeth2018@gmail.com>
2025-11-12 12:47:51 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+let user = 'john';
+user = '23';
+let number = 10;
+let isTrue = true;
+let arr = ['john', 'joy'];
+let arrr = [10, 20, 30];
+let arrrr = [true, false, false];
+let rep = [[10], [20]];
+let all = [[10], ['ada'], [true]];
+//# sourceMappingURL=index.js.map
---
+++ index.ts
@@ -0,0 +1,12 @@
+let user: string = 'john';
+
+user = '23';
+
+let number: number = 10;
+let isTrue: boolean = true;
+let arr: string[] = ['john', 'joy']
+let arrr: number[] = [10, 20, 30];
+let arrrr: boolean[] = [true, false, false];
+
+let rep: number[][] = [[10], [20]];
+let all: any[][] = [[10], ['ada'], [true]];
